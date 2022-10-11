62716046df8a3c467b4e1e6f877d5de30752b35a
Чистякова <goldkiki@yandex.ru>
2022-10-11 12:36:42 +0300

    комментарии к каждой строчке

--- task_1.py
+++ task_1.py
@@ -7,10 +7,10 @@ import numpy
 
 # Эта программа рисует график функции, заданной выражением ниже
 
-if __name__ == '__main__':
-    arguments = numpy.arange(0, 200, 0.1)
-    mpp.plot(
-        arguments,
-        [math.sin(a) * math.sin(a / 20.0) for a in arguments]
+if __name__ == '__main__':  # начало программы что бы при импорте не активировались
+    arguments = numpy.arange(0, 200, 0.1)  # список точек от 0 до 200 с шагом 0.1
+    mpp.plot(  # строим график по точкам
+        arguments,  # сюда передаем х
+        [math.sin(a) * math.sin(a / 20.0) for a in arguments]  # здесь полуем список у
     )
     mpp.show()  # вывод картинки
