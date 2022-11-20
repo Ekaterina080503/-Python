675f9c71dd0872ed94e9e32b6cb3766f14d06afa
Чистякова <goldkiki@yandex.ru>
2022-11-20 21:25:23 +0300

    Алгоритм Евклида

---
+++ Evklid.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import math
+
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+a = int(input())
+b = int(input())
+print(gcd(a, b))
+print(math.gcd(a, b) == gcd(a, b))
---
+++ Evklid_extended.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        d, x, y = egcd(b % a, a)
+    return (d, y - (b // a) * x, x)
+
+
+a = int(input())
+b = int(input())
+d, x, y = egcd(a, b)
+print(d, x, y)
+print(d == a * x + b * y)
